97245679a30e59cdba9173b02e4ce5edc8da37e5
Aziz-KZ <iz.aziz.ali@gmail.com>
2025-03-10 16:33:31 +0500

    Матрицы

---
+++ 1_Python/Lesson_13_1.py
@@ -0,0 +1,24 @@
+import random
+
+def pl(t):
+    for i in t:
+        print(*i)
+
+x = int(input("Введите количество строк: "))
+y = int(input("Введите количество столбцов: "))
+
+# Генерация двух матриц со случайными числами
+matrix_1 = [[random.randint(-100, 100) for _ in range(y)] for _ in range(x)]
+matrix_2 = [[random.randint(-100, 100) for _ in range(y)] for _ in range(x)]
+
+# Сложение двух матриц
+matrix_3 = [[matrix_1[i][j] + matrix_2[i][j] for j in range(y)] for i in range(x)]
+
+print("Первая матрица:")
+pl(matrix_1)
+
+print("\nВторая матрица:")
+pl(matrix_2)
+
+print("\nРезультирующая матрица после сложения:")
+pl(matrix_3)
