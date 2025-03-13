6214b4dae58021dc60a583df1bb57ff399ed10c4
Aziz-KZ <iz.aziz.ali@gmail.com>
2025-03-13 21:32:11 +0500

    Второй урок игра

--- 2_Python_The_Game/Map.py
+++ 2_Python_The_Game/Map.py
@@ -1,3 +1,7 @@
+from utils import randbool
+from utils import randcell
+from utils import randcell2
+
 # 0 - поле
 # 1 - дерево
 # 2 - река
@@ -7,9 +11,24 @@
 CELL_TYPES = "🟩🌲🌊🏥➕"
 
 class Map:
-    # def generate_rivers():
+    def generate_river(self, l):
+        rc = randcell(self.w, self.h)
+        rx, ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l >0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.check_bounds(rx2, ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
 
-    # def generate_forest():
+
+    def generate_forest(self, r, mxr):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
 
     def print_map(self):
         print("⬛" * (self.w + 2))
@@ -32,5 +51,8 @@ class Map:
         self.cells = [[0 for i in range(w)] for j in range(h)]
 
 tmp = Map(20, 10)
-tmp.cells[1][1] = 2
+tmp.generate_forest(3,10)
+tmp.generate_river(10)
+tmp.generate_river(10)
+tmp.generate_river(10)
 tmp.print_map()
